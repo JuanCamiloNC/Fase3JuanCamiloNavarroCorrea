e619b0937054cab7b4fa2deec2657e66664aeed9
Juan camilo <navarrocorreajuancamilo@gmail.com>
2022-10-12 14:25:31 -0500

    creacion del menu

--- Fase3JuanCamiloNavarroCorrea/Form1.Designer.cs
+++ Fase3JuanCamiloNavarroCorrea/Form1.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.F_Contraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estructura de datos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Juan Camilo Navarro Correa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // F_Contraseña
+            // 
+            this.F_Contraseña.Location = new System.Drawing.Point(330, 139);
+            this.F_Contraseña.Name = "F_Contraseña";
+            this.F_Contraseña.PasswordChar = '*';
+            this.F_Contraseña.Size = new System.Drawing.Size(100, 23);
+            this.F_Contraseña.TabIndex = 4;
+            this.F_Contraseña.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.F_Contraseña);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private TextBox F_Contraseña;
     }
 }
--- Fase3JuanCamiloNavarroCorrea/Form1.cs
+++ Fase3JuanCamiloNavarroCorrea/Form1.cs
@@ -6,5 +6,25 @@ namespace Fase3JuanCamiloNavarroCorrea
         {
             InitializeComponent();
         }
+        Menu menu =new Menu();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (F_Contraseña.Text != "")
+            {
+                if (F_Contraseña.Text == "123")
+                {
+                    menu.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("La contraseña es incorrecta.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Escriba la contraseña.");
+            }
+        }
     }
 }
