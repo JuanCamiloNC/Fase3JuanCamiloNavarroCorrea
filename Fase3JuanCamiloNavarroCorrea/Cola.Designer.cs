17cf3d8210f06d9d4e79516bdc921717682f541e
Juan camilo <navarrocorreajuancamilo@gmail.com>
2022-10-18 17:15:49 -0500

    creacion del diseño cola y la clase colaJuventud

--- Fase3JuanCamiloNavarroCorrea/Cola.Designer.cs
+++ Fase3JuanCamiloNavarroCorrea/Cola.Designer.cs
@@ -28,7 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.C_Comunas = new System.Windows.Forms.ComboBox();
+            this.C_Genero = new System.Windows.Forms.ComboBox();
+            this.C_Identificacion = new System.Windows.Forms.TextBox();
+            this.C_NombreC = new System.Windows.Forms.TextBox();
+            this.C_Direccion = new System.Windows.Forms.TextBox();
+            this.DatosDeLaCola = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.registrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.DatosDeLaCola)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identificacion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre Completo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Direccion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Comunas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Genero";
+            // 
+            // C_Comunas
+            // 
+            this.C_Comunas.FormattingEnabled = true;
+            this.C_Comunas.Location = new System.Drawing.Point(226, 187);
+            this.C_Comunas.Name = "C_Comunas";
+            this.C_Comunas.Size = new System.Drawing.Size(121, 23);
+            this.C_Comunas.TabIndex = 5;
+            // 
+            // C_Genero
+            // 
+            this.C_Genero.FormattingEnabled = true;
+            this.C_Genero.Location = new System.Drawing.Point(226, 241);
+            this.C_Genero.Name = "C_Genero";
+            this.C_Genero.Size = new System.Drawing.Size(121, 23);
+            this.C_Genero.TabIndex = 6;
+            // 
+            // C_Identificacion
+            // 
+            this.C_Identificacion.Location = new System.Drawing.Point(226, 40);
+            this.C_Identificacion.Name = "C_Identificacion";
+            this.C_Identificacion.Size = new System.Drawing.Size(100, 23);
+            this.C_Identificacion.TabIndex = 7;
+            // 
+            // C_NombreC
+            // 
+            this.C_NombreC.Location = new System.Drawing.Point(226, 89);
+            this.C_NombreC.Name = "C_NombreC";
+            this.C_NombreC.Size = new System.Drawing.Size(100, 23);
+            this.C_NombreC.TabIndex = 8;
+            // 
+            // C_Direccion
+            // 
+            this.C_Direccion.Location = new System.Drawing.Point(226, 135);
+            this.C_Direccion.Name = "C_Direccion";
+            this.C_Direccion.Size = new System.Drawing.Size(100, 23);
+            this.C_Direccion.TabIndex = 9;
+            // 
+            // DatosDeLaCola
+            // 
+            this.DatosDeLaCola.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DatosDeLaCola.Location = new System.Drawing.Point(63, 288);
+            this.DatosDeLaCola.Name = "DatosDeLaCola";
+            this.DatosDeLaCola.RowTemplate.Height = 25;
+            this.DatosDeLaCola.Size = new System.Drawing.Size(674, 150);
+            this.DatosDeLaCola.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarToolStripMenuItem,
+            this.eliminarToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // registrarToolStripMenuItem
+            // 
+            this.registrarToolStripMenuItem.Name = "registrarToolStripMenuItem";
+            this.registrarToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.registrarToolStripMenuItem.Text = "Registrar";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(432, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Fecha";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(505, 48);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(232, 23);
+            this.dateTimePicker1.TabIndex = 13;
             // 
             // Cola
             // 
@@ -36,12 +192,49 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DatosDeLaCola);
+            this.Controls.Add(this.C_Direccion);
+            this.Controls.Add(this.C_NombreC);
+            this.Controls.Add(this.C_Identificacion);
+            this.Controls.Add(this.C_Genero);
+            this.Controls.Add(this.C_Comunas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Cola";
             this.Text = "Cola";
+            ((System.ComponentModel.ISupportInitialize)(this.DatosDeLaCola)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ComboBox C_Comunas;
+        private ComboBox C_Genero;
+        private TextBox C_Identificacion;
+        private TextBox C_NombreC;
+        private TextBox C_Direccion;
+        private DataGridView DatosDeLaCola;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem registrarToolStripMenuItem;
+        private ToolStripMenuItem eliminarToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
     }
 }
---
+++ Fase3JuanCamiloNavarroCorrea/ColaJuventud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase3JuanCamiloNavarroCorrea
+{
+    internal class ColaJuventud
+    {
+        //Atributos 
+        public string Identificacion { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Direccion { get; set; }
+        public string Comunas { get; set; }
+        public string Genero { get; set; } 
+
+        //Metosdos o Funciones
+        //A
+    }
+}
